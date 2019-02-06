56245063a309b2a89ba346bf631fc20cdcc9289c
daniel <danielghanbari7@gmail.com>
2019-02-06 08:54:02 +0000

    github project

--- js/my-github.js
+++ js/my-github.js
@@ -1,2 +1,20 @@
 // Write code here to communicate with Github
 
+document.getElementsByClassName("nav-link js-scroll-trigger")[1]
+.addEventListener("click",function(){
+  const post = document.getElementById("repos-list");
+  const list = document.createElement("LI");
+  const newList = post.appendChild(list);
+  
+
+  fetch("https://api.github.com/users/DanGhanbari/repos")
+    .then(res => res.json())
+    .then(data => {
+      data.forEach(users => {
+        const {name} = users;
+        result += document
+        .getElementById("repos-list")
+        .appendChild(newList).innerHTML = name;})
+  
+     });
+ });
